33cffe6f1bec20e3924f65d7d1f6308f0f385231
Joseph Segun <smartdevs17@gmail.com>
2023-02-27 23:53:16 -0800

    edit 0 file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,14 @@
 #include "main.h"
+/**
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  written by Joseph Segun
+*  Return: 0 is success
+*/
 
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
 
-/**
-*  reset_to_98 - takes the pointer and updates the value
-*  @n: int to check
-*  writtenby  Joseph Segun
-*  Return: 0 is success
-*/
+
