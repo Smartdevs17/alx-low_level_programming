9d45503ef8a91b10395ec34e1548bb127481a2de
Joseph Segun <smartdevs17@gmail.com>
2023-02-27 23:38:55 -0800

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+void reset_to_98(int *n)
+{
+    // Update the value that the pointer points to
+    *n = 98;
+}
+
+int main() 
+{
+    int number = 5;
+
+    // Print the original value of the number
+    printf("Original value of number: %d\n", number);
+
+    // Call the reset_to_98() function
+    reset_to_98(&number); 
+
+    // Print the value of the number after calling reset_to_98
+    printf("Value of number after reset_to_98(): %d\n", number); 
+
+    return 0;
+}
